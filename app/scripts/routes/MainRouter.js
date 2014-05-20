e7e2a3929105958bf8a5ec1132ae76e64981f484
Lluis M. Campos <lluiscampos@malabarraca.com>
2014-05-20 20:21:05 +0200

    yo router

---
+++ app/scripts/routes/MainRouter.js
@@ -0,0 +1,12 @@
+/*global BuenaBarraca, Backbone*/
+
+BuenaBarraca.Routers = BuenaBarraca.Routers || {};
+
+(function () {
+    'use strict';
+
+    BuenaBarraca.Routers.MainRouter = Backbone.Router.extend({
+
+    });
+
+})();
